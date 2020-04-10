09fb618c5e05827d918dc8f9b3d3cb97b0343929
Nidhi4242 <63438152+Nidhi4242@users.noreply.github.com>
2020-04-10 10:49:27 +0530

    created food.c
    program in switch case for choice of food item

---
+++ food.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    int f;
+    printf("enter any number from 1 to 5 for your surprise food item");
+    scanf("%d",&f);
+      case 1:
+         printf(" food item- Pizza \n prize-Rs239");
+      case 2:
+         printf(" food item- Burger \n prize- Rs129");
+      case 3:
+         printf(" food item- Pasta \n prize- Rs179");
+      case 4:
+         printf(" food item- French Fries \n prize- Rs99");
+      case 5:
+         printf(" food item- Sandwich \n prize- Rs149");
+      default:
+         printf(" numbers are not in between 1 to 5");
+ } 
